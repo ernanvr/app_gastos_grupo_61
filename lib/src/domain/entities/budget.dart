5154876f56e3c02c1d627a338c6491a2913d7c72
ernanvr <ernanvr@gmail.com>
2025-05-02 00:06:08 -0600

    Cambiar propiedad amount por initialAmout de entidad Budget
    Cambiar la propiedad amount por una más descriptiva "initialAmount" para
    dar una mayor claridad de su propósito.

--- lib/src/domain/entities/budget.dart
+++ lib/src/domain/entities/budget.dart
@@ -5,18 +5,18 @@ class Budget extends Equatable {
   const Budget({
     this.id,
     required this.description,
-    required this.amount,
+    required this.initialAmount,
     required this.date,
   });
 
   //Las propiedades
   final int? id;
   final String description;
-  final double amount;
+  final double initialAmount;
   final DateTime date;
 
   //Este override se debe a que se está usando Equatable
   //Que permite comparar fácilmente dos instancias de una clase
   @override
-  List<Object?> get props => [id, description, amount, date];
+  List<Object?> get props => [id, description, initialAmount, date];
 }
