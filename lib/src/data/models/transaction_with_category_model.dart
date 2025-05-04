520de12cf27707f78b84d2e5ef23a4242acd3860
ernanvr <ernanvr@gmail.com>
2025-05-04 12:09:58 -0600

    Modificar model TransactionWithCategory (agregar sentencia SQL)
    La sentencia SQL es para crear un database view a partir de las tablas
    "transaction" y "category".

--- lib/src/data/models/transaction_with_category_model.dart
+++ lib/src/data/models/transaction_with_category_model.dart
@@ -1,7 +1,21 @@
 import 'package:app_gastos_grupo_61/src/domain/entities/transaction_with_category.dart';
 import 'package:floor/floor.dart';
 
-@DatabaseView('SELECT *', viewName: 'transaction_with_category')
+@DatabaseView('''
+  SELECT
+      T.id,
+      T.categoryId,
+      C.name AS categoryName, -- Seleccionamos el nombre de la categoría y lo renombramos
+      T.budgetId,
+      T.description,
+      T.amount,
+      T.date,
+      T.isIncome
+  FROM
+      transaction AS T -- Tabla de transacciones (alias T)
+  INNER JOIN
+      category AS C ON T.categoryId = C.id; -- Unimos con la tabla de categorías (alias C) donde el ID de la categoría coincide con el categoryId de la transacción. Usamos INNER JOIN porque una transacción debe tener una categoría asociada para aparecer en esta vista.
+  ''', viewName: 'transaction_with_category')
 class TransactionWithCategoryModel extends TransactionWithCategory {
   const TransactionWithCategoryModel({
     super.id,
