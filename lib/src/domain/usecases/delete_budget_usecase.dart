87ebe61d06d4816809b4eb1ca718a87e82865369
ernanvr <ernanvr@gmail.com>
2025-05-03 20:42:41 -0600

    Corregir error de la clase DeleteBudgetUseCase
    El LSP indicaba un error en la clase relacionado con los tipos de
    paramétros

--- lib/src/domain/usecases/delete_budget_usecase.dart
+++ lib/src/domain/usecases/delete_budget_usecase.dart
@@ -1,14 +1,16 @@
 import 'package:app_gastos_grupo_61/core/helpers/classes.dart';
 import 'package:app_gastos_grupo_61/core/helpers/types.dart';
+import 'package:app_gastos_grupo_61/src/domain/entities/budget.dart';
 import 'package:app_gastos_grupo_61/src/domain/repository/budget_repository.dart';
 
-class DeleteBudgetUsecase extends UseCaseWithParams<void, int> {
+class DeleteBudgetUsecase extends UseCaseWithParams<int, Budget> {
   final BudgetRepository repository;
 
   DeleteBudgetUsecase({required this.repository});
 
   @override
-  ResultFuture<void> call(int params) {
+  ResultFuture<int> call(Budget params) {
     return repository.deleteBudget(params);
   }
 }
+
